02eb72b8779a5ec666ee9ec4bdccb750935c12d3
init.defaultBranch <benlimpic@gmail.com>
2022-10-24 13:08:32 -0700

    Fixed Reset / Save Buttons

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,9 @@
 import { useStore } from '../hooks/useStore'
 
 export const Menu = () => {
-  const [saveWorld, resetWorld] = useStore((state) => [state.save, state.saveWorld])
+  const [saveWorld, resetWorld] = useStore((state) => [state.saveWorld, state.resetWorld])
   return ( <div className='menu absolute'>
-    <button className='text' onClick={() => saveWorld()}>Save</button>
-    <button className='text' onClick={() => resetWorld()}>Reset</button>
+    <button onClick={() => saveWorld()}>Save</button>
+    <button onClick={() => resetWorld()}>Reset</button>
   </div>)
 }
